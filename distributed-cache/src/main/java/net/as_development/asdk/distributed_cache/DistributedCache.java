e3fbc1e4c5eac773b2fa39d61aa5f8bc806027a5
Andreas Schlüns <andreas.schluens@as-development.net>
2016-06-14 09:07:33 +0200

    distributed-cache : ad license header; small fixes and debug out

--- distributed-cache/src/main/java/net/as_development/asdk/distributed_cache/DistributedCache.java
+++ distributed-cache/src/main/java/net/as_development/asdk/distributed_cache/DistributedCache.java
@@ -1,3 +1,29 @@
+/**
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+ */
 package net.as_development.asdk.distributed_cache;
 
 import java.util.ArrayList;
@@ -159,7 +185,7 @@ public class DistributedCache implements Observer< Message >
 		if (m_iChannel == null)
 			throw new RuntimeException ("["+sME+"] Not connected.");
 
-		System.out.println("... ["+sME+"] send out : '"+aMsg+"'");
+//		System.out.println("... ["+sME+"] send out : '"+aMsg+"'");
 		aMsg.setSender(sME);
 		m_iChannel.send(aMsg);
 	}
@@ -184,7 +210,7 @@ public class DistributedCache implements Observer< Message >
 		final String                sKey   = aSet.getKey  ();
 		final String                sValue = aSet.getValue();
 
-		System.out.println("["+sME+"] set : '"+sKey+"' = '"+sValue+"'");
+//		System.out.println("["+sME+"] set : '"+sKey+"' = '"+sValue+"'");
 		aCache.put(sKey, sValue);
 	}
 	
--- distributed-cache/src/main/java/net/as_development/asdk/distributed_cache/impl/channel/UnicastChannel.java
+++ distributed-cache/src/main/java/net/as_development/asdk/distributed_cache/impl/channel/UnicastChannel.java
@@ -1,3 +1,29 @@
+/**
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+ */
 package net.as_development.asdk.distributed_cache.impl.channel;
 
 import java.io.DataInputStream;
@@ -72,7 +98,7 @@ public class UnicastChannel extends    ObservableBase< Message >
 			m_aClientSocket = aClientSocket;
 		}
 		
-		System.out.println ("["+sME+"] connected to '"+sAddress+":"+nPort+"'");
+//		System.out.println ("["+sME+"] connected to '"+sAddress+":"+nPort+"'");
 		m_aAddress = aAddress;
 		m_nPort    = nPort   ;
 	}
@@ -111,14 +137,78 @@ public class UnicastChannel extends    ObservableBase< Message >
 		if (m_aClientSocket == null)
 			throw new RuntimeException ("["+sME+"] Not connected.");
 
-		if (m_aClientOut == null)
-			m_aClientOut = new DataOutputStream (m_aClientSocket.getOutputStream());
-
 		aMsg.setSender(sME);
 		final String sMsg = Message.serialize(aMsg);
-		m_aClientOut.writeUTF(sMsg);
+
+		for (int i=0; i<10; ++i)
+		{
+			try
+			{
+				final boolean NO_FORCE = false;
+				reconnect (NO_FORCE);
+				
+				if (m_aClientOut == null)
+					m_aClientOut = new DataOutputStream (m_aClientSocket.getOutputStream());
+		
+				m_aClientOut.writeUTF(sMsg);
+				break;
+			}
+			catch (final Throwable ex)
+			{
+				final boolean FORCE = true;
+				reconnect(FORCE);
+			}
+		}
 	}
 	
+	//-------------------------------------------------------------------------
+	private synchronized void reconnect (final boolean bForced)
+	    throws Exception
+	{
+		final DistributedCacheConfig aConfig  = m_aConfig;
+		final ERunMode               eRunMode = aConfig.getRunMode();
+
+		if (eRunMode == ERunMode.E_SERVER)
+		{
+			if (bForced)
+			{
+				disconnect();
+			}
+			else
+			if (
+				(m_aServerSocket != null    ) &&
+				(m_aServerSocket.isClosed ())
+			   )
+			{
+				m_aServerSocket = null;
+			}
+
+			// no else
+			if (m_aServerSocket == null)
+				connect ();
+		}
+		else
+		if (eRunMode == ERunMode.E_CLIENT)
+		{
+			if (bForced)
+			{
+				disconnect();
+			}
+			else
+			if (
+				(m_aClientSocket != null    ) &&
+				(m_aClientSocket.isClosed ())
+			   )
+			{
+				m_aClientSocket = null;
+			}
+			
+			// no else
+			if (m_aClientSocket == null)
+				connect ();
+		}
+	}
+		
 	//-------------------------------------------------------------------------
 	private synchronized void impl_startServer (final ServerSocket aSocket)
 		throws Exception
@@ -161,7 +251,7 @@ public class UnicastChannel extends    ObservableBase< Message >
 	private void impl_handleClient (final Socket aSocket)
 	    throws Exception
 	{
-		System.out.println("server : new client connected ...");
+//		System.out.println("server : new client connected ...");
 		
 		final String          sME         = m_sSenderId;
 		final ExecutorService aThreadPool = mem_ClientThreadPool ();
@@ -187,7 +277,7 @@ public class UnicastChannel extends    ObservableBase< Message >
 				}
 				catch (EOFException exEOF)
 				{
-					// expected ;-)
+ 					// expected ;-)
 				}
 				catch (Throwable ex)
 				{
